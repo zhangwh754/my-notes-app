bac9f449ff42c1a11b07622d239bdc75f50cd5da
zhang <zhangwh754@163.com>
2026-01-01 20:15:05 +0800

    feat: use react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,23 @@
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./route";
 import "./style/index.css";
-import { StrictMode } from "react";
 
 const root = document.getElementById("root")!;
 
+// Create a client
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(root).render(
   <StrictMode>
-    <div className="p-20 h-screen">
-      <div className="h-full rounded-2xl shadow-2xl overflow-hidden">
-        <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <div className="p-20 h-screen">
+        <div className="h-full rounded-2xl shadow-2xl overflow-hidden">
+          <RouterProvider router={router} />
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   </StrictMode>
 );
